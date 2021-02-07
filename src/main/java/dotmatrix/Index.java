e35b3b819d9d9b3d80ee4eb9f8ef831fb1fca024
afdj <ahmadfadlydziljalal@gmail.com>
2021-02-08 06:03:54 +0700

    UI updated

--- src/main/java/dotmatrix/Index.java
+++ src/main/java/dotmatrix/Index.java
@@ -5,18 +5,8 @@
  */
 package dotmatrix;
 
-import java.awt.BorderLayout;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import simple.escp.Template;
-import simple.escp.json.JsonTemplate;
-import simple.escp.swing.PrintPreviewPane;
+import java.io.File;
+import javax.swing.JFileChooser;
 
 /**
  *
@@ -24,36 +14,10 @@ import simple.escp.swing.PrintPreviewPane;
  */
 public class Index extends javax.swing.JFrame {
 
-    public Index() throws IOException, URISyntaxException {
+    public Index()  {
         
-        Template template = new JsonTemplate(Thread.currentThread().
-                getContextClassLoader().
-                getResource("template.json").
-                toURI()
-        );
         
-        Map<String, Object> value = new HashMap<>();
-        value.put("invoiceNo", "INVC-00001");
-        List<Map<String, Object>> tables = new ArrayList<>();
-         
-        for (int i = 0; i < 5; i++) {
-            Map<String, Object> line = new HashMap<>();
-            line.put("code", String.format("CODE-%d", i));
-            line.put("name", String.format("Product Random %d", i));
-            line.put("qty", String.format("%d", i * i));
-            tables.add(line);
-        }
-        value.put("table_source", tables);
-               
-       
-        PrintPreviewPane printPreview = new PrintPreviewPane(template,
-            value, null);                                                 
-        getContentPane().setLayout(new BorderLayout());
-        getContentPane().add(printPreview, BorderLayout.CENTER);          
-
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        pack();
-        
+        initComponents();
         
     }
 
@@ -66,22 +30,62 @@ public class Index extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1 = new javax.swing.JButton();
+        jTextFieldPathFile = new javax.swing.JTextField();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jButton1.setText("Pilih File");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jMenu1.setText("File");
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Edit");
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jButton1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jTextFieldPathFile, javax.swing.GroupLayout.PREFERRED_SIZE, 433, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(28, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jTextFieldPathFile, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(352, Short.MAX_VALUE))
         );
 
-        setSize(new java.awt.Dimension(416, 339));
+        setSize(new java.awt.Dimension(568, 447));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        
+        JFileChooser jfc = new JFileChooser();
+        jfc.showOpenDialog(null);
+        
+        File file = jfc.getSelectedFile();
+        jTextFieldPathFile.setText(file.getAbsolutePath());
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -99,15 +103,19 @@ public class Index extends javax.swing.JFrame {
         }
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> {
-            try {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
                 new Index().setVisible(true);
-            } catch (IOException | URISyntaxException ex) {
-                Logger.getLogger(Index.class.getName()).log(Level.SEVERE, null, ex);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JTextField jTextFieldPathFile;
     // End of variables declaration//GEN-END:variables
 }
